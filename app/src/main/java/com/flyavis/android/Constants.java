8cb499fc2df6f314ecdaf7c38015b0d241f01724
Felix Wang <grimmi75380@gmail.com>
2019-01-28 16:08:58 +0800

    feat: Integrate the Firebase Authentication
    Include Google & Facebook login
    
    Signed-off-by: Felix Wang <grimmi75380@gmail.com>

--- app/src/main/java/com/flyavis/android/Constants.java
+++ app/src/main/java/com/flyavis/android/Constants.java
@@ -2,4 +2,7 @@ package com.flyavis.android;
 
 public class Constants {
     public static final String DB_NAME = "flyavis-db";
+    // Choose an arbitrary request code value
+    public static final int RC_SIGN_IN = 123;
+
 }
--- app/src/main/java/com/flyavis/android/ui/StartActivity.java
+++ app/src/main/java/com/flyavis/android/ui/StartActivity.java
@@ -5,14 +5,76 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.flyavis.android.R;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static com.flyavis.android.Constants.RC_SIGN_IN;
+
 public class StartActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //For login check & onboarding experience
         super.onCreate(savedInstanceState);
-        Intent activityIntent;
-        activityIntent = new Intent(this, MainActivity.class);
+        checkLoginStatus();
+    }
+
+    private void checkLoginStatus() {
+        FirebaseApp.initializeApp(this);
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+            launchMainActivity();
+        } else {
+            // not signed in
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.GoogleBuilder().build(),
+                    new AuthUI.IdpConfig.FacebookBuilder().build()
+            );
+            // Create and launchMainActivity sign-in intent
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .setIsSmartLockEnabled(false)
+                            .setTheme(R.style.AppTheme)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                launchMainActivity();
+
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
+    private void launchMainActivity() {
+        Intent activityIntent = new Intent(this, MainActivity.class);
         startActivity(activityIntent);
         finish();
     }
+
 }
